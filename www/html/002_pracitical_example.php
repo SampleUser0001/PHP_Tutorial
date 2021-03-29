1baeea4880bbea39126c17ccf397ce7d51fb7b03
SatoruTanaka <ittimfn@gmail.com>
2021-03-29 10:54:51 +0900

    実用的な例

--- www/html/002_pracitical_example.php
+++ www/html/002_pracitical_example.php
@@ -7,6 +7,28 @@
   <body>
     <?php
       echo $_SERVER['HTTP_USER_AGENT'];
+    ?> <br />
+    <hr>
+    <?php
+      if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+        echo 'あなたはInternet Explorerを使用しています。<br />';
+      } else {
+        echo 'あなたはInternet Explorerを使用していません。<br />';
+      }
+    ?>
+    <hr>
+    <?php
+      if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+    ?>
+        <h3>strposが非falseを返しました</h3>
+        <center><b>あなたはInternet Explorerを使用しています</b></center>
+    <?php
+      } else {
+    ?>
+        <h3>strposがfalseを返しました</h3>
+        <center><b>あなたはInternet Explorerを使用していません</b></center>
+    <?php
+      }
     ?>
   </body>
 </html>
